597caeea18b61e5187c381e41ae4d75beeaa5b46
Luke <yl4107@nyu.edu>
2023-07-05 22:19:28 -0400

    small tweaks

--- Assets/Scripts/EnableObjects/LivableObject.cs
+++ Assets/Scripts/EnableObjects/LivableObject.cs
@@ -10,7 +10,9 @@ public class LivableObject : MonoBehaviour
     [SerializeField] protected bool hasParent;
     [SerializeField] protected float minDist;
 
+
     protected Material mat;
+    [SerializeField] protected Renderer rend;
     [SerializeField] public bool activated;
     [SerializeField] protected bool firstActivated; 
     [SerializeField] protected float matColorVal;
@@ -20,7 +22,11 @@ public class LivableObject : MonoBehaviour
     protected virtual void Start()
     {
         player = GameObject.Find("Player").transform;
-        mat = GetComponent<Renderer>().material;
+        if (GetComponent<Renderer>() != null)
+        {
+            rend = GetComponent<Renderer>();
+        }
+        mat = rend.material;
         mat.EnableKeyword("_WhiteDegree");
         matColorVal = 1;
         if (transform.parent != null)
@@ -78,7 +84,7 @@ public class LivableObject : MonoBehaviour
 
     protected virtual bool IsInView()
     {
-        Vector3 pointOnScreen = Camera.main.WorldToScreenPoint(GetComponent<Renderer>().bounds.center);
+        Vector3 pointOnScreen = Camera.main.WorldToScreenPoint(rend.bounds.center);
 
         //Is in front
         if (pointOnScreen.z < 0)
@@ -99,7 +105,7 @@ public class LivableObject : MonoBehaviour
         Vector3 heading = gameObject.transform.position - Camera.main.transform.position;
         Vector3 direction = heading.normalized;// / heading.magnitude;
 
-        if (Physics.Linecast(Camera.main.transform.position, GetComponent<Renderer>().bounds.center, out hit))
+        if (Physics.Linecast(Camera.main.transform.position, rend.bounds.center, out hit))
         {
             if (hit.transform.name != gameObject.name)
             {

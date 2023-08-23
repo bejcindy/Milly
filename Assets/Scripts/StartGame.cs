7f4517210e570c3dc95a916fe32efb39a286ab13
Luke <yl4107@nyu.edu>
2023-08-23 17:03:00 -0400

    start

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -1,18 +1,38 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class StartGame : MonoBehaviour
 {
+    public PlayerMovement playerMovement;
+    public PlayerCam playerCam;
+    public CinemachineVirtualCamera playerCinemachine;
+    public bool startSequence;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (startSequence)
+        {
+            playerMovement.enabled = false;
+            playerCam.enabled = false;
+        }
+        else
+        {
+            playerMovement.enabled = true;
+            playerCam.enabled=true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ActivateGame()
+    {
+        playerMovement.enabled = true;
+        playerCam.enabled = true;
     }
 }

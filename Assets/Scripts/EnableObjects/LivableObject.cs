384e6f817b53c6c3ce3cfc06c123d327d7ad4d3d
Cindy Fan <fanj696@newschool.edu>
2023-08-29 18:13:28 -0400

    changed raycast

--- Assets/Scripts/EnableObjects/LivableObject.cs
+++ Assets/Scripts/EnableObjects/LivableObject.cs
@@ -37,7 +37,7 @@ public class LivableObject : MonoBehaviour
 
     public bool npc;
 
-
+    bool[] checkBoundVisible;
 
     protected virtual void Start()
     {
@@ -53,7 +53,7 @@ public class LivableObject : MonoBehaviour
             mat.EnableKeyword("_WhiteDegree");
             mat.SetFloat("_WhiteDegree", 1);
         }
-
+        checkBoundVisible = new bool[8];
         matColorVal = 1;
         playerCam = GameObject.Find("PlayerCinemachine").GetComponent<CinemachineVirtualCamera>();
     }
@@ -152,23 +152,64 @@ public class LivableObject : MonoBehaviour
         }
         else
         {
-            if ((pointOnScreen.x < Screen.width * 0.2f) || (pointOnScreen.x > Screen.width * 0.8f) ||
-               (pointOnScreen.y < Screen.height * 0.2f) || (pointOnScreen.y > Screen.height * 0.8f))
+            //if ((pointOnScreen.x < Screen.width * 0.2f) || (pointOnScreen.x > Screen.width * 0.8f) ||
+            //   (pointOnScreen.y < Screen.height * 0.2f) || (pointOnScreen.y > Screen.height * 0.8f))
+            //{
+            //    return false;
+            //}
+            int pointsInScreen = 0;
+            Vector3 pointA = rend.bounds.min;
+            Vector3 pointB = rend.bounds.min + new Vector3(rend.bounds.size.x , 0, 0);
+            Vector3 pointC = rend.bounds.min + new Vector3(0, rend.bounds.size.y , 0);
+            Vector3 pointD = rend.bounds.min + new Vector3(0, 0, rend.bounds.size.z );
+            Vector3 pointE = rend.bounds.max - new Vector3(rend.bounds.size.x , 0, 0);
+            Vector3 pointF = rend.bounds.max - new Vector3(0, rend.bounds.size.y , 0);
+            Vector3 pointG = rend.bounds.max - new Vector3(0, 0, rend.bounds.size.z );
+            Vector3 pointH = rend.bounds.max;
+            
+
+            checkBoundVisible[0] = CheckPointInView(pointA);
+            checkBoundVisible[1] = CheckPointInView(pointB);
+            checkBoundVisible[2] = CheckPointInView(pointC);
+            checkBoundVisible[3] = CheckPointInView(pointD);
+            checkBoundVisible[4] = CheckPointInView(pointE);
+            checkBoundVisible[5] = CheckPointInView(pointF);
+            checkBoundVisible[6] = CheckPointInView(pointG);
+            checkBoundVisible[7] = CheckPointInView(pointH);
+            
+            for (int i = 0; i < checkBoundVisible.Length; i++)
             {
-                return false;
+                if (checkBoundVisible[i])
+                    pointsInScreen++;
+                else
+                    pointsInScreen--;
             }
+            
+            if (pointsInScreen < 3)
+                return false;
         }
 
 
-        RaycastHit hit;
-        if (Physics.Raycast(Camera.main.transform.position, GetComponent<Renderer>().bounds.center, out hit))
-        {
-            if (hit.collider.name != gameObject.name && !hit.collider.CompareTag("Player"))
-            {
-                Debug.Log(gameObject.name + "blocked by " + hit.collider.name);
-                return false;
-            }
+        //RaycastHit hit;
+        //if (Physics.Raycast(Camera.main.transform.position, GetComponent<Renderer>().bounds.center, out hit))
+        //{
+        //    if (hit.collider.name != gameObject.name && !hit.collider.CompareTag("Player"))
+        //    {
+        //        Debug.Log(gameObject.name + "blocked by " + hit.collider.name);
+        //        return false;
+        //    }
 
+        //}
+        return true;
+    }
+
+    bool CheckPointInView(Vector3 pointPos)
+    {
+        Vector3 pointOnScreen = Camera.main.WorldToScreenPoint(pointPos);
+        if ((pointOnScreen.x < Screen.width * 0.05f) || (pointOnScreen.x > Screen.width * 0.95f) ||
+           (pointOnScreen.y < Screen.height * 0.05f) || (pointOnScreen.y > Screen.height * 0.95f))
+        {
+            return false;
         }
         return true;
     }

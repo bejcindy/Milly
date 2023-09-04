425822960bbadba5a99a5cee36161ff04cfc0c6a
Luke <yl4107@nyu.edu>
2023-09-04 15:34:23 -0400

    sitting functions

--- Assets/Scripts/PlayerSittingSwitch.cs
+++ Assets/Scripts/PlayerSittingSwitch.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerSittingSwitch : MonoBehaviour
 {
+
+    public CinemachineVirtualCamera playerMainCam;
+    public CinemachineVirtualCamera playerSitCam;
+
     // Start is called before the first frame update
     void Start()
     {

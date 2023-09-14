f89b470a760703aee27bd3c20e59ec6b69af4d09
Cindy Fan <fanj696@newschool.edu>
2023-09-14 18:27:53 -0400

    trying

---
+++ Assets/Cindy/materialtest.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class materialtest : MonoBehaviour
+{
+    Material correct;
+    Material wrong;
+    Material[] correctMaterials;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        correctMaterials = Resources.LoadAll<Material>("Assets/3D");
+        //correctMaterials= AssetDatabase.LoadAllAssetsAtPath<Material>("Assets/3D")
+        Debug.Log(correctMaterials.Length);
+        if (gameObject.GetComponent<Renderer>())
+        {
+            wrong = GetComponent<Renderer>().material;
+            Debug.Log("wrong:" + wrong.name);
+            if (wrong.name.Contains("Instance"))
+            {
+                string tempName = wrong.name.Replace("(Instance)", "");
+                string realName = tempName.Replace(" ", "");
+                Debug.Log(realName);
+                foreach (Material mat in correctMaterials)
+                {
+                    if (mat.name == realName)
+                    {
+                        Debug.Log("Found this: " + mat.name);
+                        correct = mat;
+                    }
+                        
+                }
+                GetComponent<Renderer>().material = null;
+                GetComponent<Renderer>().sharedMaterial = correct;
+                Destroy(wrong);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

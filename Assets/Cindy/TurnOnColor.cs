257e3ef32bb287063bffe4f41d500a2ae3341df8
Cindy Fan <fanj696@newschool.edu>
2023-09-24 15:46:14 -0400

    turn on lights as well

--- Assets/Cindy/TurnOnColor.cs
+++ Assets/Cindy/TurnOnColor.cs
@@ -6,18 +6,33 @@ using UnityEngine;
 public class TurnOnColor : MonoBehaviour
 {
     Renderer[] childrenRends;
+    Light[] childrenLights;
+    List<Light> originallyOffLights;
 
 
     private void OnEnable()
     {
         if (childrenRends == null)
             childrenRends = GetComponentsInChildren<Renderer>();
+        if (childrenLights == null)
+            childrenLights = GetComponentsInChildren<Light>();
+        if (originallyOffLights == null)
+            originallyOffLights = new List<Light>();
         foreach (Renderer r in childrenRends)
         {
             Material mat = r.sharedMaterial;
-            mat.SetFloat("_WhiteDegree", 0);
+            if(mat.HasProperty("_WhiteDegree"))
+                mat.SetFloat("_WhiteDegree", 0);
         }
-
+        foreach(Light l in childrenLights)
+        {
+            if (!l.gameObject.activeSelf)
+            {
+                if (!originallyOffLights.Contains(l))
+                    originallyOffLights.Add(l);
+                l.gameObject.SetActive(true);
+            }
+        }
             
             //Debug.Log(mat.name);
 
@@ -27,8 +42,13 @@ public class TurnOnColor : MonoBehaviour
         foreach (Renderer r in childrenRends)
         {
             Material mat = r.sharedMaterial;
-            mat.SetFloat("_WhiteDegree", 1);
+            if (mat.HasProperty("_WhiteDegree"))
+                mat.SetFloat("_WhiteDegree", 1);
             //Debug.Log(mat.name);
+        }
+        foreach(Light l in originallyOffLights)
+        {
+            l.gameObject.SetActive(false);
         }
     }
 }

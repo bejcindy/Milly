bd701c5d284dcb2c645ef420142157b6d79b0e93
Cindy Fan <fanj696@newschool.edu>
2023-10-04 17:12:04 -0400

    trash can working

--- Assets/Scripts/EnableObjects/KickObject.cs
+++ Assets/Scripts/EnableObjects/KickObject.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,39 +13,88 @@ public class KickObject : MonoBehaviour
     public GameObject kickHint;
     public GameObject instantiatedHint;
     public Vector3 hintPosition;
+
+    bool isVisible, interactable;
+    Collider coll;
+    bool hinted;
+    bool activated,firstActivated;
+    GameObject usefulChild;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         player = GameObject.Find("Player").transform;
+        coll = GetComponent<Collider>();
+        coll.enabled = false;
+        rb.isKinematic = true;
+        foreach(Transform child in transform)
+        {
+            if (child.gameObject.GetComponent<GroupMaster>())
+                usefulChild = child.gameObject;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (activateTrigger.interactable)
+
+        if (isColliderVisible(coll))
+        {
+            isVisible = IsInView();
+        }
+        else
+            isVisible = false;
+
+        DetectInteractable();
+
+        if (interactable)
         {
             if(instantiatedHint == null)
             {
-                instantiatedHint = Instantiate(kickHint, hintPosition, Quaternion.identity);
+                //instantiatedHint = Instantiate(kickHint, hintPosition, Quaternion.identity);
                 //instantiatedHint.transform.SetParent(GameObject.Find("Canvas").transform);
-
+                if (!hinted)
+                {
+                    DataHolder.ShowHint(DataHolder.hints.kickHint);
+                    player.GetComponent<PlayerHolding>().kickableObj = gameObject;
+                    hinted = true;
+                }
             }
 
             if (Input.GetKeyDown(KeyCode.Q))
             {
                 Destroy(instantiatedHint);
-                activateTrigger.activated = true;
-                transform.GetChild(0).gameObject.GetComponent<MeshCollider>().enabled = false;
-                transform.GetChild(1).gameObject.GetComponent<MeshCollider>().enabled = false;
-                GetComponent<CapsuleCollider>().enabled = true;
+                activated = true;
+                //transform.GetChild(0).gameObject.GetComponent<MeshCollider>().enabled = false;
+                transform.GetChild(1).gameObject.GetComponent<Collider>().enabled = false;
+                coll.enabled = true;
                 rb.isKinematic = false;
-                rb.AddForce(player.forward * 5);
+                rb.AddForce(player.forward * 20f,ForceMode.Impulse);
                 Invoke(nameof(DeactivateEnabler), 2f);
+                if (hinted)
+                {
+                    DataHolder.HideHint();
+                    player.GetComponent<PlayerHolding>().kickableObj = null;
+                    hinted = false;
+                }
             }
         }
         else
         {
-            Destroy(instantiatedHint);
+            //Destroy(instantiatedHint);
+            if (hinted)
+            {
+                DataHolder.HideHint();
+                player.GetComponent<PlayerHolding>().kickableObj = null;
+                hinted = false;
+            }
+        }
+
+        if (activated)
+        {
+            usefulChild.GetComponent<GroupMaster>().activateAll = true;
+            GetComponent<KickObject>().enabled = false;
+
         }
 
     }
@@ -52,5 +102,66 @@ public class KickObject : MonoBehaviour
     public void DeactivateEnabler()
     {
         activateTrigger.enabled = false;
+    }
+
+ 
+
+
+    void DetectInteractable()
+    {
+        if (Vector3.Distance(transform.position, player.position) <= 3)
+        {
+            if (isVisible)
+                interactable = true;
+            else
+                interactable = false;
+        }
+        else
+        {
+            interactable = false;
+        }
+
+    }
+    bool IsInView()
+    {
+        Vector3 pointOnScreen = Camera.main.WorldToScreenPoint(coll.bounds.center);
+
+
+        //Is in front
+        if (pointOnScreen.z < 0)
+        {
+            return false;
+        }
+
+        
+            if ((pointOnScreen.x < Screen.width * 0.2f) || (pointOnScreen.x > Screen.width * 0.8f) ||
+               (pointOnScreen.y < Screen.height * 0.2f) || (pointOnScreen.y > Screen.height * 0.8f))
+            {
+                return false;
+            }
+
+            if (GetComponent<Renderer>())
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(Camera.main.transform.position - new Vector3(0, 0.1f, 0), GetComponent<Renderer>().bounds.center - (Camera.main.transform.position - new Vector3(0, 0.1f, 0)), out hit, Mathf.Infinity, Physics.AllLayers, QueryTriggerInteraction.Ignore))
+                {
+                    //if (hit.collider)
+                    //    Debug.Log(gameObject.name+" raycast hit this: "+hit.collider.gameObject.name);
+                    if (hit.collider.name != gameObject.name && !hit.collider.CompareTag("Player"))
+                    {
+
+                        return false;
+                    }
+
+                }
+            }
+        
+
+        return true;
+    }
+
+    bool isColliderVisible(Collider col)
+    {
+        return GeometryUtility.TestPlanesAABB(GeometryUtility.CalculateFrustumPlanes(Camera.main), col.bounds);
     }
 }

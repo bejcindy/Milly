07212d05be8088a6f2d2f632f81923ddd1ec3843
Luke <yiyanl@uci.edu>
2023-10-16 21:31:45 -0400

    xixi in game

---
+++ Assets/Scripts/EnableObjects/DoorDetector1.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorDetector1 : MonoBehaviour
+{
+    public float autoCloseTimer;
+    private float autoCloseTimerVal;
+    public bool hasHuman;
+    public Door myDoor;
+    void Start()
+    {
+        autoCloseTimerVal = 10f;
+        myDoor = transform.parent.GetComponent<Door>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasHuman && myDoor.doorOpen)
+        {
+            if (autoCloseTimer > 0)
+                autoCloseTimer -= Time.deltaTime;
+            else
+            {
+                myDoor.CloseDoor();
+                autoCloseTimer = autoCloseTimerVal;
+            }
+
+        }
+        else
+        {
+            autoCloseTimer = autoCloseTimerVal;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("NPC") || other.CompareTag("Player"))
+        {
+            hasHuman = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("NPC") || other.CompareTag("Player"))
+        {
+            hasHuman = false;
+        }
+    }
+
+
+}

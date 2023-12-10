9838dd25afdc0fee09b938a70ddcce5065531f55
Luke <yiyanl@uci.edu>
2023-12-10 16:13:50 -0500

    gloria drop fixed

--- Assets/Scripts/Progression/InitialActivation.cs
+++ Assets/Scripts/Progression/InitialActivation.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class InitialActivation : MonoBehaviour
 {
     public List<Transform> iniLightObj;
+    public List<Transform> iniOffObj;
     bool initialOn;
     // Start is called before the first frame update
     void Start()
@@ -12,6 +13,11 @@ public class InitialActivation : MonoBehaviour
         foreach (Transform obj in iniLightObj)
         {
             ActivateAll(obj);
+        }
+
+        foreach(Transform obj in iniOffObj)
+        {
+            DeactivateAll(obj);
         }
 
     }
@@ -58,9 +64,51 @@ public class InitialActivation : MonoBehaviour
 
     }
 
+    void DeactivateAll(Transform obj)
+    {
+        if (obj.GetComponent<Renderer>() != null)
+        {
+            Material mat = obj.GetComponent<Renderer>().material;
+            if (mat.HasProperty("_WhiteDegree"))
+            {
+                mat.EnableKeyword("_WhiteDegree");
+                if (mat.GetFloat("_WhiteDegree") < 1)
+                    TurnOffColor(mat);
+            }
+
+        }
+
+        foreach (Transform child in obj)
+        {
+            if (child.childCount <= 0 && child.GetComponent<Renderer>() != null)
+            {
+                Material childMat = child.GetComponent<Renderer>().material;
+                if (childMat.HasProperty("_WhiteDegree"))
+                {
+                    childMat.EnableKeyword("_WhiteDegree");
+                    if (childMat.GetFloat("_WhiteDegree") < 1)
+                        TurnOffColor(childMat);
+                }
+
+            }
+            else
+            {
+                DeactivateAll(child);
+            }
+
+
+        }
+
+    }
+
     void TurnOnColor(Material material)
     {
         material.SetFloat("_WhiteDegree", 0);
 
+    }
+
+    void TurnOffColor(Material material)
+    {
+        material.SetFloat("_WhiteDegree", 1);
     }
 }

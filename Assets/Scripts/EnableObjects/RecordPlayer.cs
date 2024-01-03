54294e398d8d19ca04c3433a545314543d5d4907
Luke <yiyanl@uci.edu>
2024-01-02 22:30:52 -0500

    record player done

--- Assets/Scripts/EnableObjects/RecordPlayer.cs
+++ Assets/Scripts/EnableObjects/RecordPlayer.cs
@@ -1,11 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VInspector;
 
-public class RecordPlayer : PickUpObject
+public class RecordPlayer : LivableObject
 {
+    [Foldout("RecordPlayer")]
     public bool hasRecord;
-    public Lid recordLid;
-    public bool lidOpen;
+    public Vinyl currentRecord;
+
     public Transform recordPos;
+
+
+    public Quaternion playingRot;
+    public Quaternion stopRot;
+    public bool isPlaying;
+
+    bool moving;
+    protected override void Start()
+    {
+        base.Start();
+        isPlaying = true;
+        hasRecord = true;
+        playingRot = transform.localRotation;
+        stopRot = Quaternion.Euler(0f, 0f, 0f);
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (interactable)
+        {
+            if (!moving)
+            {
+                rend.gameObject.layer = 9;
+                if (Input.GetMouseButtonDown(0))
+                {
+                    TriggerPlay();
+                }
+            }
+            else
+            {
+                rend.gameObject.layer = 0;
+            }
+
+        }
+        else
+        {
+            rend.gameObject.layer = 0;
+        }
+    }
+
+    void TriggerPlay()
+    {
+        if (isPlaying)
+        {
+            isPlaying = false;
+            StartCoroutine(LerpRotation(stopRot, 1f));
+        }
+        else
+        {
+
+            StartCoroutine(LerpRotation(playingRot, 1f));
+        }
+    }
+
+
+    IEnumerator LerpRotation(Quaternion targetRot, float duration)
+    {
+        moving = true;
+        float time = 0;
+        Quaternion startValue = transform.localRotation;
+        while (time < duration)
+        {
+            transform.localRotation = Quaternion.Lerp(startValue, targetRot, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.localRotation = targetRot;
+        moving = false;
+
+        if(targetRot == playingRot)
+        {
+            isPlaying = true;
+        }
+    }
+
 }

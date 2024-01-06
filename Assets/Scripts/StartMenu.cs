f3f52428f4e24a09c5fd7a3fa55f7744f92c4fd8
Zhaijnpng <zhaijnpng@gmail.com>
2024-01-05 21:03:46 -0500

    start motherfucking menu

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -1,26 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
     public bool startAllowed;
+    public Animator cinemachineAnim;
+    public Animator startButtonAnim;
+    public Animator quitButtonAnim;
+    public EventSystem eventSystem;
+    Animator canvasAnim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        canvasAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (startAllowed)
+        if (cinemachineAnim.isActiveAndEnabled)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if(cinemachineAnim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
             {
-                GetComponent<Animator>().SetTrigger("Start");
+                LoadIzakayaScene();
             }
+        }
+
+        if (Input.anyKeyDown)
+        {
+            StartGame();
         }
     }
 
@@ -29,8 +40,36 @@ public class StartMenu : MonoBehaviour
         startAllowed = true;
     }
 
+    public void EnableButtonAnim()
+    {
+        startButtonAnim.enabled = true;
+        quitButtonAnim.enabled = true;
+    }
+
+
+    public void DisableButtonAnim()
+    {
+        eventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(null);
+        //startButtonAnim.enabled = false;
+        //quitButtonAnim.enabled = false;
+    }
     public void LoadIzakayaScene()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void StartGame()
+    {
+        canvasAnim.SetTrigger("Start");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartCameraLerp()
+    {
+        cinemachineAnim.enabled = true;
     }
 }

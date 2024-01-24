371f9b3e2a0537f08da2f8cce6783e81007a2a9a
Luke <yiyanl@uci.edu>
2024-01-23 19:09:32 -0500

    shit is fucked

--- Assets/Scripts/EnableObjects/PizzaBoard.cs
+++ Assets/Scripts/EnableObjects/PizzaBoard.cs
@@ -5,6 +5,31 @@ using TMPro;
 
 public class PizzaBoard : LookingObject
 {
-    public TextMeshProUGUI titleText;
+    public TextMeshPro titleText;
+    public float titleTextTargetVal;
+
+
+    protected override void Start()
+    {
+        base.Start();
+        titleTextTargetVal = titleText.color.a;
+        Color temp = titleText.color;
+        temp.a = 0;
+        titleText.color = temp;
+    }
+    protected override void Update()
+    {
+        base.Update();
+
+        if (activated)
+        {
+            if(titleText.color.a < titleTextTargetVal)
+            {
+                Color temp = titleText.color;
+                temp.a += 0.1f * fadeInterval * Time.deltaTime;
+                titleText.color = temp;
+            }
+        }
+    }
 
 }
